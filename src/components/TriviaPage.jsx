7ba240f031df0616d0a658a4a0c3d1d5d4e3262f
maorazoulay <maorazoulay@gmail.com>
2023-07-08 20:18:11 -0400

    removed dummy data from main file

--- src/components/TriviaPage.jsx
+++ src/components/TriviaPage.jsx
@@ -8,64 +8,6 @@ const questionCount = 5
 const apiUrl = `https://opentdb.com/api.php?amount=${questionCount}`
 let correctAnswerCount = 0
 
-const dummyQuestions = [
-    {
-        id: 1,
-        title: "It's nothing",
-        answers: [
-            {id:"ghjw", text: "nothing", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "everything", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "money", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "all", backgroundClass: "", isChecked: false}],
-        correctAnswer: {id:"ghjw", text: "nothing", backgroundClass: "", isChecked: false},
-        markedAnswer: {},
-    }, 
-    {
-        id: 2,
-        title: "it's money",
-        answers: [
-            {id:nanoid(), text: "nothing", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "everything", backgroundClass: "", isChecked: false}, 
-            {id:"iuweyr", text: "money", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "all", backgroundClass: "", isChecked: false}],
-        correctAnswer: {id:"iuweyr", text: "money", backgroundClass: "", isChecked: false},
-        markedAnswer: {},
-    },
-    {
-        id: 3,
-        title: "it's everything",
-        answers: [
-            {id:nanoid(), text: "nothing", backgroundClass: "", isChecked: false}, 
-            {id:"iuweyr7", text: "everything", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "money", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "all", backgroundClass: "", isChecked: false}],
-        correctAnswer: {id:"iuweyr7", text: "everything", backgroundClass: "", isChecked: false},
-        markedAnswer: {},
-    },
-    {
-        id: 4,
-        title: "it's all",
-        answers: [
-            {id:nanoid(), text: "nothing", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "everything", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "money", backgroundClass: "", isChecked: false}, 
-            {id:"8329fjc", text: "all", backgroundClass: "", isChecked: false}],
-        correctAnswer: {id:"8329fjc", text: "all", backgroundClass: "", isChecked: false},
-        markedAnswer: {},
-    },
-    {
-        id: 5,
-        title: "it's booga",
-        answers: [
-            {id:"kdsljf8", text: "booga", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "everything", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "money", backgroundClass: "", isChecked: false}, 
-            {id:nanoid(), text: "all", backgroundClass: "", isChecked: false}],
-        correctAnswer: {id:"kdsljf8", text: "booga", backgroundClass: "", isChecked: false},
-        markedAnswer: {},
-    }
-]
-
 export default function TriviaPage(){
     const [triviaFinished, setTriviaFinished] = useState(false)
     const [showResults, setShowResults] = useState(false)
@@ -76,11 +18,11 @@ export default function TriviaPage(){
         fetchNewQuestions()
     }, [triviaFinished])
 
-    // useEffect(() => {
-    //     if(questions.every(question => Object.keys(question.markedAnswer) > 0)){
-    //         setAllQuestionsAnswered(true)
-    //     }
-    // }, [questions])
+    useEffect(() => {
+        if(questions.every(question => Object.keys(question.markedAnswer) > 0)){
+            setAllQuestionsAnswered(true)
+        }
+    }, [questions])
 
     function handleChange(event, answerData, questionId){
         event.preventDefault()
@@ -159,7 +101,6 @@ export default function TriviaPage(){
                     markedAnswer: {}
                 })
             }
-            console.log(newQuestions)
             setQuestions(newQuestions)
         })
     }
@@ -177,7 +118,7 @@ export default function TriviaPage(){
                 {questionElements}
                 <div className='center'>
                     {!showResults && <button className='btn bigger-font' 
-                        // disabled={!allQuestionsAnswered} 
+                        disabled={!allQuestionsAnswered} 
                         onClick={handleSubmit}>Check Answers</button>}
                     {showResults && <button className='btn bigger-font' 
                         onClick={restartTrivia}>Play Again</button>}
@@ -186,6 +127,5 @@ export default function TriviaPage(){
                 </div>
             </form>
         </div>
-
     )
 }
