d4694630b3eb66aa7bc540e5d59499892c89ef10
maorazoulay <maorazoulay@gmail.com>
2023-07-10 15:11:46 -0400

    Added LoadingPage component and its css

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingPage() {
+    return (
+        <div className='loading-ctn'>
+            <h1 className='loading'>Loading Questions...</h1>
+        </div>
+    )
+}
